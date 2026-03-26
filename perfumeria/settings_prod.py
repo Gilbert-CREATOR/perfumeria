2801cdc31ff09498bd9ac8e04516a997a175733f
Gilbert Andeliz <gilbertandeliz04@gmail.com>
2026-03-26 12:57:17 -0400

    fix superuser production

--- perfumeria/settings_prod.py
+++ perfumeria/settings_prod.py
@@ -28,7 +28,7 @@ DATABASES['default']['OPTIONS'] = {
 # 📧 EMAIL
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = int(config('EMAIL_PORT'))
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')

d514a8e51e2de69ddac2e81dc4f34d09c855ff92
Gilbert Andeliz <gilbertandeliz@MacBook-Air-de-Gilbert.local>
2026-07-12 13:48:13 -0400

    Apply admin credentials during WSGI startup

--- perfumeria/wsgi.py
+++ perfumeria/wsgi.py
@@ -10,7 +10,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'perfumeria.settings_prod')
 
 application = get_wsgi_application()
+
+# Algunos servicios de Render conservan el Start Command configurado en el
+# panel e ignoran Procfile/render.yaml. Sincronizar aquí garantiza que las
+# credenciales secretas se apliquen al arrancar cualquier proceso WSGI.
+if os.environ.get('DJANGO_SUPERUSER_USERNAME') and os.environ.get('DJANGO_SUPERUSER_PASSWORD'):
+    call_command('sync_admin')
